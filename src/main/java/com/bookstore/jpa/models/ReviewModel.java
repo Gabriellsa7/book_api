9f21b30aa9fa3f7777a09dbe7959cedcf96813d9
Gabriel Santana Santos <gabrielsantanasantos798@gmail.com>
2024-08-20 01:05:02 -0300

    feat: created review model

---
+++ src/main/java/com/bookstore/jpa/models/ReviewModel.java
@@ -0,0 +1,35 @@
+package com.bookstore.jpa.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_review")
+public class ReviewModel implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String comment;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
